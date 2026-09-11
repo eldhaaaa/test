0a13e71598e504d06c26d7b310aa0d95f8855fd7
eldhaaaa <eldha2727@gmail.com>
2026-09-11 10:53:31 +0700

    test

---
+++ abc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+
+    printf("This is a test program.\n");
+    return 0;
+}
+
+
+void test() {
+    printf("This is a test function.\n  ");
+}
+
+void test2() {
+    printf("This is another test function.\n");
+}
